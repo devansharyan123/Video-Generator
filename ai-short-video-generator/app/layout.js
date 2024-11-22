3fe172b14782732f68dd5704b52d87f41d8d2191
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2024-11-23 00:18:43 +0530

    clerk auth added

--- ai-short-video-generator/app/layout.js
+++ ai-short-video-generator/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import provider from "./provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +21,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    
+      <ClerkProvider>
+        <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <provider>
+            {children}
+        </provider> 
       </body>
-    </html>
+      </html>
+      </ClerkProvider>
+   
   );
 }
--- ai-short-video-generator/app/page.js
+++ ai-short-video-generator/app/page.js
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <h1> chal bsdk</h1>
-    <Button variant="ghost">Gandu</Button>
+    
     </>
   );
 }
