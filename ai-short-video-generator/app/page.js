9a3b2e09e3a2993826e9af33b9cd2b54cd1d9247
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2024-11-26 06:51:19 +0530

    added new user and font

--- ai-short-video-generator/app/page.js
+++ ai-short-video-generator/app/page.js
@@ -1,11 +1,12 @@
 
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <h1> chal bsdk</h1>
-    
+    <UserButton />
     </>
   );
 }
