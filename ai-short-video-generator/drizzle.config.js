69a3f860be8468e6b4a05fdf3b9132e412a2f98d
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2024-12-01 08:32:11 +0530

    safe

--- ai-short-video-generator/drizzle.config.js
+++ ai-short-video-generator/drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: "postgresql://WEB%20PROJECTS_owner:LFKoc1wbPW0e@ep-shy-cloud-a5ffp9ym.us-east-2.aws.neon.tech/AI-SHORT-VIDEO-GENERATOR?sslmode=require",
+    url: NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   },
 });
